be0a3fd8a87edf429a1c002eb975367b6fc59b83
root <root@delltoya>
2014-01-09 16:09:39 +0100

    Scaffolding

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('dondetocahoyApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma',
+	  'Schibsted'
+    ];
+  });
